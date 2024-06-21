40bcf85871fb879332132ee0fd6cd1c8b4f83163
elvin sara eldho <elvinsara1999@gmail.com>
2024-06-21 15:25:14 +0530

    movie manager added

--- sample_env/movie_management/movies/models.py
+++ sample_env/movie_management/movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#MovieIno is amodel cls which is inheriting the 'Model' class. models is a module. it has a class called Model
+
+class MovieInfo(models.Model):
+    name = models.CharField(max_length=50,null=True)
+    year = models.IntegerField(null=True)
+    summary = models.TextField()
--- sample_env/movie_management/movies/views.py
+++ sample_env/movie_management/movies/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from . models import MovieInfo
 
 # Create your views here.
 
 def view_movies(request):
-    return render(request,'viewmovies.html')
+    #returns query set
+    movie_set = MovieInfo.objects.all
+    return render(request,'viewmovies.html',{'movie':movie_set})
 
 def create_movies(request):
+    if request.POST:
+        name = request.POST.get('name')
+        year = request.POST.get('year')
+        summary = request.POST.get('summary')
+        print("--------------------------------------------------")
+        print(name,year,summary)
+        movie_obj = MovieInfo(name=name,year=year,summary=summary)
+        movie_obj.save()
     return render(request,'createmovies.html')
 
 def update_movies(request):
